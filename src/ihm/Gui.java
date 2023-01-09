13b04511d16428cae9a1239afc8f95f7b6e780e4
LucienLeR0ux <lulu.worg@gmail.com~>
2023-01-09 16:31:51 +0100

    dsdsd

--- src/ihm/Gui.java
+++ src/ihm/Gui.java
@@ -78,5 +78,5 @@ public class Gui extends JFrame
     {
         this.panelPioche.piocherCarteObjectif();
     }
-    
+
 }
--- src/metier/Joueur.java
+++ src/metier/Joueur.java
@@ -105,6 +105,7 @@ public class Joueur {
         
     }
 
+
     public HashMap<String, ArrayList<Carte>> getCartes() {
         return cartes;
     }
